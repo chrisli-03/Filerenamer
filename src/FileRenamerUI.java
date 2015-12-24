1770eb8d9842d69370f4aac103ec855b10598b87
HechengLi <lihecheng93@gmail.com>
2015-12-23 23:38:24 -0500

    Revert "Revert "Added more functions""
    This reverts commit 5511d83f62fb2859cb5000b006c667dbf5d464ae.

--- src/FileRenamerUI.java
+++ src/FileRenamerUI.java
@@ -19,12 +19,15 @@ import java.util.List;
 import javax.swing.JEditorPane;
 import java.awt.Font;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 public class FileRenamerUI {
 
 	private boolean keepExtention;
 	private File fileList[];
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -58,14 +61,15 @@ public class FileRenamerUI {
 		frame.setBounds(100, 100, 500, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
 		
 		Panel fileDisplayPanel = new Panel();
-		fileDisplayPanel.setBounds(0, 1, 360, 216);
+		fileDisplayPanel.setBounds(0, 1, 289, 216);
 		frame.getContentPane().add(fileDisplayPanel);
 		fileDisplayPanel.setLayout(null);
 		
 		JScrollPane fileScrollPane = new JScrollPane();
-		fileScrollPane.setBounds(0, 0, 360, 216);
+		fileScrollPane.setBounds(0, 0, 289, 216);
 		fileDisplayPanel.add(fileScrollPane);
 	
 		JList fileDisplayList = new JList();
@@ -82,23 +86,41 @@ public class FileRenamerUI {
 		buttonDisplayPanel.add(btnRename);
 		
 		Panel optionDisplayPanel = new Panel();
-		optionDisplayPanel.setBounds(366, 1, 118, 216);
+		optionDisplayPanel.setBounds(295, 1, 189, 216);
 		frame.getContentPane().add(optionDisplayPanel);
 		optionDisplayPanel.setLayout(null);
 		
 		JTextPane readMe = new JTextPane();
 		readMe.setFont(new Font("Dialog", Font.PLAIN, 9));
-		readMe.setBounds(12, 5, 94, 97);
+		readMe.setBounds(12, 5, 167, 58);
 		optionDisplayPanel.add(readMe);
 		readMe.setText("use # to indicate where to insert sequence number. eg 'test#abc' will give 'test1abc' to 'test10abc' for 10 files");
 		
 		JEditorPane userInputFilename = new JEditorPane();
-		userInputFilename.setBounds(12, 114, 94, 24);
+		userInputFilename.setBounds(85, 74, 94, 20);
 		optionDisplayPanel.add(userInputFilename);
+		userInputFilename.setText("prefix#suffix.ext");
 		
 		JRadioButton rdbtnKeepExtension = new JRadioButton("keep extension");
-		rdbtnKeepExtension.setBounds(8, 146, 121, 26);
+		rdbtnKeepExtension.setBounds(12, 183, 167, 26);
 		optionDisplayPanel.add(rdbtnKeepExtension);
+		
+		textField = new JTextField();
+		textField.setBounds(12, 74, 65, 20);
+		optionDisplayPanel.add(textField);
+		textField.setColumns(10);
+		textField.setText("new name");
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(12, 105, 65, 20);
+		optionDisplayPanel.add(textField_1);
+		textField_1.setColumns(10);
+		textField_1.setText("starting#");
+		
+		JEditorPane startingSequenceNumber = new JEditorPane();
+		startingSequenceNumber.setBounds(85, 105, 94, 20);
+		optionDisplayPanel.add(startingSequenceNumber);
+		startingSequenceNumber.setText("1");
 		
 		SimpleFileChooser JFileChooser = new SimpleFileChooser ();
 		
@@ -106,7 +128,9 @@ public class FileRenamerUI {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				fileList = JFileChooser.run();
-				updateFileDisplay(fileDisplayList);
+				if (fileList != null) {
+					updateFileDisplay(fileDisplayList);
+				}
 			}
 		});
 		
@@ -114,26 +138,41 @@ public class FileRenamerUI {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				String newFilename = userInputFilename.getText();
-				if (newFilename.length() > 1) {
+				
+				if (newFilename.length() > 0) {
+					keepExtention = rdbtnKeepExtension.isSelected();
+					int numberOfFiles = fileList.length;
+					int numberOfDigits = countDigits(numberOfFiles);
+					
 					int sequenceNumberPosition = getSequenceNumberPosition(newFilename);
-					System.out.println(sequenceNumberPosition);
 					List<String> nameList = new ArrayList<String>();
-					for (int i = 0; i < fileList.length; i++) {
+					for (int i = 0; i < numberOfFiles; i++) {
 						String prefix = newFilename.substring(0, sequenceNumberPosition);
 						String suffix = "";
-						if (sequenceNumberPosition < newFilename.length()-2) {
+						String extension = "";
+						if (keepExtention) {
+							int dot = fileList[i].getName().lastIndexOf('.');
+							if (dot > 0) {
+							    extension = fileList[i].getName().substring(dot);
+							}
+						}
+						if (sequenceNumberPosition < newFilename.length()-1) {
 							suffix = newFilename.substring(sequenceNumberPosition+1, newFilename.length());
 						}
-						String sequenceNumber = Integer.toString(i+1);
-						if (i < 9) {
+						String sequenceNumber = Integer.toString(i);
+						int offset = Integer.parseInt(startingSequenceNumber.getText());
+						if (offset > 0) {
+							sequenceNumber = Integer.toString(i+offset);;
+						}
+						while (sequenceNumber.length() < numberOfDigits) {
 							sequenceNumber = "0" + sequenceNumber;
 						}
-						nameList.add(prefix + sequenceNumber + suffix);
+						nameList.add(prefix + sequenceNumber + suffix + extension);
 						
 					}
 					String[] newNamelist = new String[nameList.size()];
 					nameList.toArray(newNamelist);
-					for (int i = 0; i < fileList.length; i++) {
+					for (int i = 0; i < numberOfFiles; i++) {
 						Path source = Paths.get(fileList[i].getAbsolutePath());
 						try {
 							source = Files.move(source, source.resolveSibling(newNamelist[i]));
@@ -143,12 +182,7 @@ public class FileRenamerUI {
 							e.printStackTrace();
 						}
 					}
-					for (int i = 0; i < fileList.length; i++) {
-						System.out.println(fileList[i].getName());
-					}
 					updateFileDisplay(fileDisplayList);
-				} else {
-					System.out.println("0");
 				}
 			}
 		});
@@ -177,5 +211,15 @@ public class FileRenamerUI {
 		}
 		if (numberOfIndicator > 1) pos = str.length();
 		return pos;
+	}
+	
+	private int countDigits(int n) {
+		int digits= 0;
+		for (int i = 1; i < n; i = i*10) {
+			if (n/i >= 1) {
+				digits++;
+			}
+		}
+		return digits;
 	}
 }
